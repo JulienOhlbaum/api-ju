794c97bb757088204a75911a259d060c56b1f8c2
Julien Ohlbaum <julien.ohlbaum@hotmail.fr>
2022-12-22 22:40:26 +0100

    connection with DB

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const Thing = require('./models/thing');
+
+mongoose.connect('mongodb+srv://julien_93600:Alcocebre218039@cluster0.v66fxfu.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 const app = express();
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -8,26 +20,20 @@ app.use((req, res, next) => {
   next();
 });
 
+app.post('/api', (req, res, next) => {
+  const thing = new Thing({
+    //title: req.body.title ...
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
 app.use('/api', (req, res, next) => {
-  const stuff = [
-    {
-      _id: 'oeihfzeoi',
-      title: 'Mon premier objet',
-      description: 'Les infos de mon premier objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 4900,
-      userId: 'qsomihvqios',
-    },
-    {
-      _id: 'oeihfzeomoihi',
-      title: 'Mon deuxième objet',
-      description: 'Les infos de mon deuxième objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 2900,
-      userId: 'qsomihvqios',
-    },
-  ];
-  res.status(200).json(stuff);
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
